62c11d9ade9d17c1659e5533b891446da9b9ad9b
LudovicGendre <ludovic.gendre@g-mail.fr>
2020-07-07 09:56:28 +0200

    Test affichage dynamique d'un element du tableau

--- front-end/fetch.js
+++ front-end/fetch.js
@@ -7,6 +7,11 @@ function test(){
   })
   .then(function(data) {
       console.log(data);
+      console.log(data[2].name);
+      document.getElementById("monDiv").innerHTML=data[2].name;
+      document.getElementById("maDescription").innerHTML=data[2].description;
+      document.getElementById("monImage").setAttribute("src",data[2].imageUrl);
+      document.getElementById("monPrix").innerHTML=data[2].price + ' €';
   })
   .catch(function(error){
       console.log(error)
