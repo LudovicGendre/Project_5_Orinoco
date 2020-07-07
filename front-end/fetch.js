3feec11bacc9c403963bb11cbdbff83ac8afbf50
LudovicGendre <ludovic.gendre@g-mail.fr>
2020-07-07 10:05:53 +0200

    [fetch.js] Creation d'un boucle console.log

--- front-end/fetch.js
+++ front-end/fetch.js
@@ -7,11 +7,14 @@ function test(){
   })
   .then(function(data) {
       console.log(data);
-      console.log(data[2].name);
       document.getElementById("monDiv").innerHTML=data[2].name;
       document.getElementById("maDescription").innerHTML=data[2].description;
       document.getElementById("monImage").setAttribute("src",data[2].imageUrl);
       document.getElementById("monPrix").innerHTML=data[2].price + ' €';
+
+      for(let datas of data ){
+        console.log('Nom: ' + datas.name + "\n" + "Description :" + datas.description + "\n" + "Prix : " + datas.price + " €");
+      };
   })
   .catch(function(error){
       console.log(error)
