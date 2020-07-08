8569e1cb0c83da02438aafa46e7f3bb30f77469f
LudovicGendre <ludovic.gendre@g-mail.fr>
2020-07-08 11:47:04 +0200

    [Product.js] Recuperation de l'id dans l'url

--- front-end/product.js
+++ front-end/product.js
@@ -1,3 +1,10 @@
+// Recuperation de l'id dans l'url
+const urlParams = new URLSearchParams(window.location.search)
+let idProduct = urlParams.get("id")
+console.log(idProduct);
+
+
+
 fetch('http://localhost:3000/api/teddies')
   .then(function (response) {
     return response.json();
