00f2bde30378f2952f0e5ca3e52ec9b504e2e22f
LudovicGendre <ludovic.gendre@g-mail.fr>
2020-07-09 09:26:08 +0200

    Creation forEach affichage differente couleur teddies

--- front-end/product.js
+++ front-end/product.js
@@ -21,16 +21,43 @@ fetch('http://localhost:3000/api/teddies/' + idProduct)
   })
   .then(function (data) {
 
-    let divCol = createElement("div")
-    divCol.classList.add("col-6")
+    let divColLeft = createElement("div")
+    divColLeft.classList.add("col-md-4")
+    divColLeft.classList.add("order-md-1")
+
+    let divColRight = createElement("div")
+    divColRight.classList.add("col-md-8")
+    divColRight.classList.add("order-md-2")
+
+    let divCardImg = createElement("img")
+    divCardImg.classList.add("TeddiesPicture")
+    divCardImg.src = data.imageUrl
+    divCardImg.setAttribute("alt", "Ours en peluche")
+    divCardImg.style.width = "100%"
+    divCardImg.style.margin = "auto"
+    appendChild(divColLeft, divCardImg)
     
     let divTitle = createElement("h5")
-      divTitle.classList.add("card-title")
-      divTitle.innerHTML = data.name
-    appendChild(divCol, divTitle)
-    
+    divTitle.classList.add("card-title")
+    divTitle.innerHTML = data.name
+    appendChild(divColRight, divTitle)
 
-    appendChild(containerProduct, divCol)
+    data.colors.forEach(element => {
+      let divColor = createElement("button")
+      divColor.classList.add("btn")
+      divColor.classList.add('btn-primary')
+      divColor.innerHTML = element
+      appendChild(divColRight, divColor)
+    });
+    
+    let divDescription = createElement("p")
+    divDescription.classList.add("texte")
+    divDescription.innerHTML = data.description
+    appendChild(divColRight, divDescription)
+
+    
+    appendChild(containerProduct, divColRight)
+    appendChild(containerProduct, divColLeft)
       console.log(data);
   })
   // Affiche l'erreur
