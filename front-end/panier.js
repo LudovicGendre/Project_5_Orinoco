5dc32bb7a73ec2ed66b10965929a6483d59dafcb
LudovicGendre <ludovic.gendre@g-mail.fr>
2020-07-10 13:46:32 +0200

    [Panier.js] Essai d'affichage de la commande

--- front-end/panier.js
+++ front-end/panier.js
@@ -24,21 +24,30 @@ function createElement(element) {
           Object.values(cartItems).map(item => {
 
             let divCol = createElement("div")
-            divCol.classList.add("col")
+            divCol.classList.add("row")
             appendChild(productContainer, divCol)
 
-            let divP = createElement("p")
-            divP.classList.add("texte")
+            let divP = createElement("div")
+            divP.classList.add("col")
             divP.innerHTML += item.name
             appendChild(divCol, divP)
 
 
-            let divCol2 = createElement("p")
-            divCol2.classList.add("texte")
+            let divCol2 = createElement("div")
+            divCol2.classList.add("col-6")
             divCol2.innerHTML += item.price
             appendChild(divCol, divCol2)
+
+            let divCol3 = createElement("div")
+            divCol3.classList.add("col")
+            appendChild(divCol, divCol3)
            
-            
+            let divImg = createElement("img")
+            divImg.classList.add("teddiesImages")
+            divImg.src += item.image
+            divImg.style.width= "60%"
+            divImg.style.margin="auto"
+            appendChild(divCol3,divImg)
             
           });
 
