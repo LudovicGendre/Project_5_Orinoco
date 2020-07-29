28dd2aee3cc47eb0b9f80be1cd2ee188338edf22
LudovicGendre <ludovic.gendre@g-mail.fr>
2020-07-29 15:27:32 +0200

    [confirm.js] Confirmation commande, affichage email + heure de commande

--- front-end/confirm.js
+++ front-end/confirm.js
@@ -1,19 +1,39 @@
 var client = localStorage.getItem('client');
 var totalCost = localStorage.getItem('totalCost');
 
+var now = new Date();
+
+var heure = now.getHours();
+var minutes = now.getMinutes();
+
 function displayConfirm(){
     let confirmClient = localStorage.getItem('client');
     confirmClient = JSON.parse(confirmClient);
 
     let confirmContainer = document.querySelector("#confirmContainer");
+    let emailContainer =  document.querySelector("#emailContainer");
 
-    let divCol = createElement("div")
-    divCol.classList.add("col")
-    divCol.innerHTML = confirmClient.prenom
-    appendChild(confirmContainer, divCol )
+
+    let h2Username = createElement("h2")
+    h2Username.classList.add("username")
+    h2Username.innerHTML = "Merci de votre commande " + confirmClient.prenom
+    appendChild(confirmContainer, h2Username )
+
+
+
+    let pEmail = createElement("p")
+    pEmail.classList.add("text")
+    pEmail.innerHTML = "Un e-mail de confirmation a été envoyé à " + confirmClient.email
+    appendChild(emailContainer, pEmail)
+
+    let pHour = createElement("p")
+    pHour.classList.add("text")
+    pHour.innerHTML = "Votre commande a été effectuée à " + heure + "h" + minutes 
+    appendChild(emailContainer, pHour)
 
     console.log(confirmClient)
     console.log(confirmClient.adress)
+    console.log('votre commande a été effectué à '+heure+"H"+minutes)
 }
 
 displayConfirm();
