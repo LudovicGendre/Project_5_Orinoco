098660ac771b4a6093c393452241643187720f7d
LudovicGendre <ludovic.gendre@g-mail.fr>
2020-07-29 16:24:39 +0200

    [Utils.js] Add notification panier rempli

--- front-end/utils.js
+++ front-end/utils.js
@@ -6,3 +6,18 @@ function createElement(element) {
   function appendChild(parent, element) {
     return parent.appendChild(element);
   }
+
+
+  function fullCart(){
+    let teddies = localStorage.getItem('teddies');
+    if(teddies){
+      setAddItem();
+    }
+  }
+
+  function setAddItem() {
+    const addItem = document.querySelector('.check_cart');
+    addItem.className = "btn btn-success ml-auto mr-2 check_cart add"
+  }
+
+  fullCart();
