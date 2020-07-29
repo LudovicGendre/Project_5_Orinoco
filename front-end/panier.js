405771f2a234d9dcec03eb49b9c121f2d6661c23
LudovicGendre <ludovic.gendre@g-mail.fr>
2020-07-29 11:06:45 +0200

    [Panier] Recuperation des données depuis le forms

--- front-end/panier.js
+++ front-end/panier.js
@@ -69,6 +69,95 @@ function totalCart() {
   appendChild(subTotal, divPrice)
 }
 
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const nickname = document.getElementById('nickname');
+const email = document.getElementById('email');
+const adress = document.getElementById('adress');
+const adressMore = document.getElementById('adressMore');
+const city = document.getElementById('city');
+
+form.addEventListener('submit', (e) => {
+
+  e.preventDefault();
+  checkInputs();
+  
+});
+
+function checkInputs() {
+  // recupere les valeurs des forms
+  const usernameValue = username.value.trim();
+  const nicknameValue = nickname.value.trim();
+  const emailValue = email.value.trim();
+  const adressValue = adress.value.trim();
+  const adressMoreValue = adressMore.value.trim();
+  const cityValue = city.value.trim();
+
+  const clientObject = { 
+    prenom: usernameValue, 
+    nom:nicknameValue, 
+    email:emailValue,
+    adress:adressValue,
+    city:cityValue
+  };
+  
+
+  if (usernameValue === ''){
+    // affichage erreur
+    setErrorFor(username, 'Prenom ne peut être vide');
+  } else {
+    // affichage sucess
+    setSucessFor(username);
+  }
+  if(nicknameValue === ''){
+    setErrorFor(nickname, 'Nom ne peut être vide');
+  } else {
+    // affichage sucess
+    setSucessFor(nickname);
+  }
+  if(emailValue === '') {
+    setErrorFor(email, 'Email ne peut être vide');
+  }else if(!isEmail(emailValue)){
+    setErrorFor(email, 'Email n"est pas valide');
+  } else {
+    setSucessFor(email);
+  }
+  if(adressValue === ''){
+    setErrorFor(adress, 'Adresse ne peut être vide');
+  } else {
+    // affichage sucess
+    setSucessFor(adress);
+  }
+  if(cityValue === ''){
+    setErrorFor(city, 'Ville ne peut être vide');
+  } else {
+    // affichage sucess
+    setSucessFor(city);
+  }
+    localStorage.setItem('client', JSON.stringify(clientObject));
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; // .form-control
+  const small = formControl.querySelector('small');
+// Ajout message d'erreur dans le small
+  small.innerText = message;
+// Ajout de la classe error
+  formControl.className = 'form-control error';
+}
+
+function setSucessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control sucess';
+}
+
+function isEmail(email) {
+  return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+
+
 // Appel des fonctions
 
 totalCart();
